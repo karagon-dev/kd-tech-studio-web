56781bb583f838d3fc9c6d54d3cfb35b97e6585a
Kenny Aragon <kenny.ara.cas.ka@gmail.com>
2026-05-16 10:17:07 -0600

    refactor: remove unused Sparkles import from Hero component

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { ArrowRight, Sparkles } from 'lucide-react';
+import { ArrowRight } from 'lucide-react';
 import './Hero.scss';
 
 export function Hero() {
